b862ed493f2bba7f3cb5c1f11cca814e285e0d80
zi-nan1124 <115333723+zi-nan1124@users.noreply.github.com>
2025-04-10 15:34:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-CONFIG = {
-    "rpc_url":  "https://lingering-wispy-voice.solana-mainnet.quiknode.pro/6e5fc0da5f61cc165757bcd9c453ab31ba36419a/",
-    "rpc_url1": "https://lingering-wispy-voice.solana-mainnet.quiknode.pro/6e5fc0da5f61cc165757bcd9c453ab31ba36419a/",
-    "rpc_url2": "https://lingering-fragrant-hexagon.solana-mainnet.quiknode.pro/e27d3b258cfb3d3c3f808767fe98ed8fa189e38e/",
-    "rpc_url3": "https://wild-boldest-rain.solana-mainnet.quiknode.pro/b95f33839916945a42159c53ceab4d7468a51a69/",
-    "input_path": "../INPUT",
-    "output_path": "../RESULT",  # 新增的配置项
-    "log_enabled": True,
-    "HELIU_APIKEY":"9ab6766f-ff7d-4256-92d1-beb442440800",
-    "BIRDEYE_APIKEY": "1417024180f241ac91e88350dc2a1058",
-    "telegram_token": "8021447065:AAE5Bze4RSCxTMeJf9O51u-zZ3vr4OJE_Hw",
 
-}
