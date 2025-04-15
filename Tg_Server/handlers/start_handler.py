a9fcb647a83bfd56c13703aae4cceabd6b6c0790
zi-nan1124 <614276714@qq.com>
2025-04-15 15:29:00 +0800

    update of start page

--- Tg_Server/handlers/start_handler.py
+++ Tg_Server/handlers/start_handler.py
@@ -14,6 +14,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "可用命令包括：\n"
         "`/profit 钱包地址` - 查询 7 日盈利\n"
         "`/decode 签名1 签名2 ...` - 解码交易\n"
-        "`/findtoptrader token地址` - 查询 Token 的顶级交易者盈利",
+        "`/findtoptrader token地址` - 查询 Token 的顶级交易者盈利\n"
+        "`/alpha` - 查询当前 BirdEye 热门 Token 并过滤已监控地址\n"
+        "`/alpha_mute 地址 原因` - 将地址添加进监控屏蔽列表",
         parse_mode="Markdown"
     )
