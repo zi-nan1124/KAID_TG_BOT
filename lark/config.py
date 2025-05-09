02aac3de4deaba34363534d8b7b06556d62740aa
zi-nan1124 <115333723+zi-nan1124@users.noreply.github.com>
2025-05-09 18:17:47 +0800

    Update config.py

--- lark/config.py
+++ lark/config.py
@@ -5,7 +5,7 @@ Configuration settings for lark_server framework.
 
 # webhook url
 test_url = "https://open.larksuite.com/open-apis/bot/v2/hook/6ef1a3db-2a07-480c-9d76-79e7466618ab"
-pyp_url = "https://open.larksuite.com/open-apis/bot/v2/hook/4054874f-9732-4ea0-8c2b-08ad0613551c"
+pvp_url = "https://open.larksuite.com/open-apis/bot/v2/hook/4054874f-9732-4ea0-8c2b-08ad0613551c"
 
 # BlockTime_Fetcher related setting
 BlockTime_Fetcher_MESSAGE_PUSH_INTERVAL = 600  # e.g., push every hour
